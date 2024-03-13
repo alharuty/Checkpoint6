19db841c9a51c472c7376a9af51e560f310488af
allharut <132786929+alharuty@users.noreply.github.com>
2024-03-13 18:22:57 +0100

    Add files via upload

---
+++ checkpoint6.py
@@ -0,0 +1,16 @@
+class Usuario:
+    def __init__(self, usuario, contraseña):
+        self.usuario = usuario
+        self.contraseña = contraseña
+
+mi_usuario = Usuario("AllaDevCamp", "HelloWorld!")
+
+print("Nombre de usuario:", mi_usuario.usuario)
+print("Contraseña:", mi_usuario.contraseña)
+
+''''
+ LINK AL DOCUMENTO DE LAS PREGUNTAS:
+
+https://docs.google.com/document/d/15PmdXav_skYQl3oVlDhE712lEGGV5vDbLdJBIrA1DYE/edit?usp=sharing
+
+'''
